da1a55192439cd8216775d8cb1474321fe70ea2e
Ramil2003 <theluckyfeed1@gmail.com>
2022-04-12 07:53:34 +0300

    Add Card model

--- main_app/models.py
+++ main_app/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 
 class Card(models.Model):
+    """Модель карточки товара"""
     article = models.IntegerField(default=0)
     title = models.CharField(default='', max_length=255)
     price_without_disc = models.IntegerField(default=0)
     price_with_disc = models.IntegerField(default=0)
     brand = models.CharField(default='', max_length=255)
     provider = models.CharField(default='', max_length=255)
+
+    def __str__(self):
+        return f'{self.title} - {self.article}'
