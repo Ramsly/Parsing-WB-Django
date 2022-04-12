14d3559ab5e54aa6aab55fa7406b8561919ca590
Ramil2003 <theluckyfeed1@gmail.com>
2022-04-12 07:51:59 +0300

    Add Card model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Card(models.Model):
+    article = models.IntegerField(default=0)
+    title = models.CharField(default='', max_length=255)
+    price_without_disc = models.IntegerField(default=0)
+    price_with_disc = models.IntegerField(default=0)
+    brand = models.CharField(default='', max_length=255)
+    provider = models.CharField(default='', max_length=255)
