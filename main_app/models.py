993448741e127db177858d0d9a58f4e86a872036
Ramil2003 <theluckyfeed1@gmail.com>
2022-04-14 11:07:01 +0300

    Add blank=True

--- main_app/models.py
+++ main_app/models.py
@@ -7,11 +7,11 @@ class Card(models.Model):
     Поля: article, title, price_without_disc, price_with_disc, brand, provider.
     """
     article = models.IntegerField(default=0)
-    title = models.CharField(default='', max_length=255)
-    price_without_disc = models.IntegerField(default=0)
-    price_with_disc = models.IntegerField(default=0)
-    brand = models.CharField(default='', max_length=255)
-    provider = models.CharField(default='', max_length=255)
+    title = models.CharField(default='', max_length=255, blank=True)
+    price_without_disc = models.IntegerField(default=0, blank=True)
+    price_with_disc = models.IntegerField(default=0, blank=True)
+    brand = models.CharField(default='', max_length=255, blank=True)
+    provider = models.CharField(default='', max_length=255, blank=True)
 
     def __str__(self):
         return f'{self.title} - {self.article}'
