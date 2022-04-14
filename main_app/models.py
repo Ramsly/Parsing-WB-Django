f6922fa486b8fc9e4f487530daaaba0cc8b0ea79
Ramil2003 <theluckyfeed1@gmail.com>
2022-04-14 11:20:16 +0300

    Delete editable=False

--- main_app/models.py
+++ main_app/models.py
@@ -7,12 +7,12 @@ class Card(models.Model):
     Поля: article, title, price_without_disc, price_with_disc, brand, provider.
     """
     article = models.IntegerField(default=0)
-    title = models.CharField(default='', max_length=255, blank=True, editable=False)
-    price_without_disc = models.IntegerField(default=0, blank=True, editable=False)
-    price_with_disc = models.IntegerField(default=0, blank=True, editable=False)
-    brand = models.CharField(default='', max_length=255, blank=True, editable=False)
-    provider = models.CharField(default='', max_length=255, blank=True, editable=False)
-    date = models.DateField(auto_now_add=True, editable=False)
+    title = models.CharField(default='', max_length=255, blank=True)
+    price_without_disc = models.IntegerField(default=0, blank=True)
+    price_with_disc = models.IntegerField(default=0, blank=True)
+    brand = models.CharField(default='', max_length=255, blank=True)
+    provider = models.CharField(default='', max_length=255, blank=True)
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.title} - {self.article}'
