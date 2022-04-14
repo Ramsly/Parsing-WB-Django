b0ec50f6a3100fb271cfa6c86938635f2d415279
Ramil2003 <theluckyfeed1@gmail.com>
2022-04-14 11:03:15 +0300

    Parsing all articles from bd

--- main_app/tasks.py
+++ main_app/tasks.py
@@ -24,33 +24,34 @@ def parse():
 
     Ничего не возвращает.
     """
-    soup = Bs(get_html('https://www.wildberries.ru/catalog/8151147/detail.aspx'), 'html.parser')
-    items = soup.find_all('div', class_='same-part-kt__info-wrap')
-    info = []
-    for item in items:
-        info.append(
-            {'price_with_disc': int(soup.find('span', class_='price-block__final-price')
-                                    .get_text(strip=True)
-                                    .replace("₽", "")
-                                    .replace("\xa0", "")),
+    for card in Card.objects.all():
+        soup = Bs(get_html(f'https://www.wildberries.ru/catalog/{card.article}/detail.aspx'), 'html.parser')
+        items = soup.find_all('div', class_='same-part-kt__info-wrap')
+        info = []
+        for item in items:
+            info.append(
+                {'price_with_disc': int(soup.find('span', class_='price-block__final-price')
+                                        .get_text(strip=True)
+                                        .replace("₽", "")
+                                        .replace("\xa0", "")),
 
-             'price_without_disc': int(soup.find('del', class_='price-block__old-price')
-                                       .get_text(strip=True)
-                                       .replace("₽", "")
-                                       .replace("\xa0", "")),
+                 'price_without_disc': int(soup.find('del', class_='price-block__old-price')
+                                           .get_text(strip=True)
+                                           .replace("₽", "")
+                                           .replace("\xa0", "")),
 
-             'brand': [i.span.text for i in soup.select('.same-part-kt__header')][0],
-             'title': [i.text for i in soup.select('.same-part-kt__header span')][1],
-             'article': int(soup.find('span', id='productNmId').get_text(strip=True)),
-             # TODO: 'provider': soup.select_one('.tooltipster-content p')
-             })
-    for data in info:
-        Card.objects.create(article=data['article'],
-                            title=data['title'],
-                            price_without_disc=data['price_without_disc'],
-                            price_with_disc=data['price_with_disc'],
-                            brand=data['brand'],
-                            provider='Me')
+                 'brand': [i.span.text for i in soup.select('.same-part-kt__header')][0],
+                 'title': [i.text for i in soup.select('.same-part-kt__header span')][1],
+                 'article': int(soup.find('span', id='productNmId').get_text(strip=True)),
+                 # TODO: 'provider': soup.select_one('.tooltipster-content p')
+                 })
+        for data in info:
+            Card.objects.create(article=data['article'],
+                                title=data['title'],
+                                price_without_disc=data['price_without_disc'],
+                                price_with_disc=data['price_with_disc'],
+                                brand=data['brand'],
+                                provider='Me')
 
 
 # TODO: Add filters to drf
