d794e2cafccf0ab9eee0d511010ca32829557ee1
Ramil2003 <theluckyfeed1@gmail.com>
2022-04-14 10:59:01 +0300

    Add filters

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Card
 from .serializers import CardSerializer
@@ -8,3 +10,6 @@ class CardModelViewSet(viewsets.ModelViewSet):
     """ModelViewSet для модели Card."""
     queryset = Card.objects.all()
     serializer_class = CardSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filter_fields = ['article', 'brand']
+    ordering_fields = ['article', 'brand']
